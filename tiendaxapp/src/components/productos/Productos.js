d88288f459216d4f81478f3a1626a3d7bfe664e9
Ignacio Pujols <49131161+ignemdev@users.noreply.github.com>
2022-02-25 09:36:57 -0400

    react router installed

---
+++ tiendaxapp/src/components/productos/Productos.js
@@ -0,0 +1,7 @@
+function Productos() {
+    return (
+        <div>Index de productos</div>
+    )
+}
+
+export default Productos;
--- tiendaxapp/src/index.js
+++ tiendaxapp/src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Productos } from './components/productos/Productos'
+
+const Routing = () => {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/productos" component={Productos} />
+      </Switch>
+      <Footer />
+    </Router>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
