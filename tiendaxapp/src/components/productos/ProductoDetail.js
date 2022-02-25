c2ec950aef300994bcc8bd885029ed0d111ee6bf
Ignacio Morel P <ignaciomp.dev@gmail.com>
2022-02-25 15:50:36 -0400

    eliminacion de imagen

--- tiendaxapp/src/components/productos/ProductoDetail.js
+++ tiendaxapp/src/components/productos/ProductoDetail.js
@@ -11,12 +11,12 @@ export default function ProductoDetail() {
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardActionArea>
-                <CardMedia
+                {/* <CardMedia
                     component="img"
                     height="140"
                     image="/static/images/cards/contemplative-reptile.jpg"
                     alt="green iguana"
-                />
+                /> */}
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         Lizard
