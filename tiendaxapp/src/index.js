0abed7aaf383c3c35d2227edfcaf70b32efca70b
Ignacio Morel P <ignaciomp.dev@gmail.com>
2022-02-25 10:15:51 -0400

    styling and routing added

--- tiendaxapp/src/index.js
+++ tiendaxapp/src/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Productos } from './components/productos/Productos'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Productos from './components/productos/Productos'
 
 const Routing = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path="/productos" component={Productos} />
-      </Switch>
-      <Footer />
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Productos />} />
+        <Route path="productos" element={<Productos />} />
+        <Route path="categorias" element={<Productos />} />
+        <Route path="*" element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );
